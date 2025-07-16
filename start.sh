14c726002d9ada9d910e0e2bc869ea87b282ec3b
goli77gal <gili77@gmail.com>
2025-07-16 19:13:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 chmod +x start.sh
 echo "Starting FastAPI on port $PORT"
-gunicorn app.main:app \
+gunicorn app.main:app
+    --chdir /app \
     --workers 2 \
     --worker-class uvicorn.workers.UvicornWorker \
     --bind 0.0.0.0:${PORT:-8000}
